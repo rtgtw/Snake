855ced2d305cd890ef85c7168ba80805c87acd9d
Remi Olateru <remiolateru@gmail.com>
2024-02-01 03:35:41 -0500

    Snake game logic

--- GameFrame.java
+++ GameFrame.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class GameFrame {
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+
+    //Create Constructors
+    GameFrame(){
+        GamePanel panel = new GamePanel();
+        this.add(panel);
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        //Fits all of the components we add into the frame
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+
+    }
+
+
+
+
 }
--- SnakeGame.java
+++ SnakeGame.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class SnakeGame {
+import javax.swing.JFrame;
+
+public class SnakeGame {
+
+    public static void main(String[] args){
+
+
+        //Create an object of Game Frame
+        GameFrame game = new GameFrame();
+
+
+    }
 }
